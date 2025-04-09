621cea3e8e24966cd98f0c08c272789b8d05f117
NguyenHoang1122 <nguyenhoang9xbn@gmail.com>
2025-04-09 11:03:18 +0700

    first commit

---
+++ thkhaibaobien/bai3.js
@@ -0,0 +1,7 @@
+let a = +prompt('Nhap gia tri a');
+let b = +prompt('Nhap gia tri b');
+if(a%b===0){
+    alert('gia tri a la boi so cua b');
+}else {
+    alert('gia tri a khong phai la boi so cua b');
+}
