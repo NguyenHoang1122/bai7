17beed09d1315538da384fa4aff3e5a23faa10f2
NguyenHoang1122 <nguyenhoang9xbn@gmail.com>
2025-04-09 17:11:33 +0700

    doitien

---
+++ doitien.js
@@ -0,0 +1,16 @@
+function check(){
+    let Amount = document.getElementById("amount").value;
+    let To = document.getElementById("to").value;
+    let From = document.getElementById("from").value;
+    let result;
+    if(From === "VND" && To === "USD"){
+        result = Amount/24 + "$";
+    }else if(From === "USD" && To === "VND"){
+        result = Amount*24 + "đ";
+    }else if(From === "USD" && To === "USD"){
+        result = Amount + "$";
+    }else{
+        result = Amount + "đ";
+    }
+    document.getElementById("result").innerHTML = result;
+}
